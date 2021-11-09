82414dbe481dcb8105e29be049d678a2fb1296ba
Michael Quinlan <mquinlan@gigsmart.com>
2021-11-09 11:07:29 -0700

    Doing stuff. Not working

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import webex from 'webextension-polyfill'
+
+const moveTab = async (offset: number) => {
+  const tabs = await webex.tabs.query({ currentWindow: true })
+  const activeTab = tabs.filter(function (tab) {
+    return tab.active;
+  })[0];
+
+  const pinnedTabs = tabs.filter(function (tab) {
+    return tab.pinned;
+  });
+
+  let newIndex = activeTab.index + offset;
+
+  if (activeTab.pinned) {
+    if (newIndex >= pinnedTabs.length) {
+      newIndex = 0;
+    } else if (newIndex < 0) {
+      newIndex = pinnedTabs.length - 1;
+    }
+  } else {
+    if (newIndex >= tabs.length) {
+      newIndex = pinnedTabs.length;
+    } else if (newIndex < pinnedTabs.length) {
+      newIndex = tabs.length - 1;
+    }
+  }
+
+  typeof activeTab.id === "number" ? await webex.tabs.move(activeTab.id, { index: newIndex }) : null;
+};
+
+const togglePinTab = async () => {
+  const tabs = await webex.tabs.query({ active: true })
+  const activeTab = tabs[0];
+
+  const properties = {
+    pinned: !activeTab.pinned
+  };
+
+  await webex.tabs.update(activeTab.id, properties);
+};
+
+const closeOtherTabs = async () => {
+  const tabs = await webex.tabs.query({ currentWindow: true, pinned: false, active: false })
+  const outer: number[] = []
+  
+  var tabIds: number[] = tabs.reduce((acc, tab) => {
+    if (typeof tab.id === "number") {
+      return [tab.id, ...acc]
+    } else {
+      return acc
+    }
+  }, outer)
+
+  webex.tabs.remove(tabIds);
+};
+
+const commandListener = async (command: string) => {
+  switch (command) {
+    case "move-left":
+      await moveTab(-1)
+      break;
+    case "move-right":
+      await moveTab(1)
+      break
+    case "pin-tab":
+      await togglePinTab()
+      break
+    case "close-other-tabs":
+      await closeOtherTabs()
+      break
+  }
+}
+
+webex.commands.onCommand.addListener(commandListener)
+
+webex.contextMenus.create({
+  id: "manage-shortcuts",
+  title: "Manage Keyboard Shortcuts",
+  contexts: ["browser_action"]
+});
---
+++ src/popup.ts
@@ -0,0 +1,26 @@
+import webex from 'webextension-polyfill'
+
+var list = document.getElementById('shortcut-list');
+
+window.onload = async () => {
+  list ? list.innerHTML = "" : null;
+  const commands = await webex.commands.getAll()
+  var inner = commands.filter(command => {
+    return command.description != "";
+  }).map(function (command) {
+    var shortcut;
+    if (command.shortcut == "") {
+      shortcut = "<i>None</i>";
+    } else {
+      shortcut = command.shortcut;
+    }
+    return "<tr><td>" + command.description + "</td><td>" + shortcut + "</td></tr>";
+  }).join("");
+
+  list ? list.innerHTML = inner : null;
+}
+
+document.getElementById('manage-keys')?.addEventListener('click', function () {
+  webex.tabs.create({ active: true, url: "chrome://extensions/configureCommands" })
+});
+
